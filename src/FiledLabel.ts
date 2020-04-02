4093c349ac04121f5046abac74ee11c6d2f5fc27
karolinaJot <amicus.karo@gmail.com>
2020-04-03 01:08:44 +0200

    working with FiledLabel - not finished

--- src/FiledLabel.ts
+++ src/FiledLabel.ts
@@ -1,1 +1,10 @@
-class FieldLabel{}
+export class FieldLabel{
+    label: string;
+    constructor(l: string) {
+        this.label =  l;
+    }
+
+    showLabel(el: HTMLElement): void{
+        el.innerHTML = this.label;
+    }
+}
--- src/InputField.ts
+++ src/InputField.ts
@@ -1,20 +1,25 @@
+import { FieldLabel } from './FiledLabel'
+
 class InputField implements Field{
     name: string;
     label: string;
     fieldType: FieldTypes = FieldTypes.TextBox ;
     value: string;
     render(el: HTMLElement): void {
-        let newDiv = document.createElement('div');
-        newDiv.innerHTML = this.label;
-        el.appendChild(newDiv);
+        let label1 = new FieldLabel(this.label);
+        label1.showLabel(el);
+        let newInput = document.createElement('input');
+        
     }
 
 }
 
-let firstQuestionLabel = document.getElementById('first-question-label');
-let testDiv = document.getElementById('test');
-console.log(typeof(testDiv));
-let inputTest = new InputField;
-inputTest.label = firstQuestionLabel.innerHTML;
-inputTest.render(testDiv);
+// let firstQuestionLabel = document.getElementById('first-question-label');
+// let testDiv = document.getElementById('test');
+// console.log(typeof(testDiv));
+// let inputTest = new InputField;
+// inputTest.label = firstQuestionLabel.innerHTML;
+// inputTest.render(testDiv);
+
+
 
