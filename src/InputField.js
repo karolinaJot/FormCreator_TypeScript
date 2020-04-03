69da537b9079d03cb5ef0986c19efe134f6f8598
karolinaJot <amicus.karo@gmail.com>
2020-04-03 22:28:10 +0200

    dealing with export/import (FieldLabel, InputField)

--- src/InputField.js
+++ src/InputField.js
@@ -1,17 +1,23 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const FiledLabel_1 = require("./FiledLabel");
 class InputField {
     constructor() {
         this.fieldType = FieldTypes.TextBox;
     }
     render(el) {
         let newDiv = document.createElement('div');
-        newDiv.innerHTML = this.label;
+        let newLabel = document.createElement('label');
+        let newInput = document.createElement('input');
+        let label1 = new FiledLabel_1.FieldLabel(this.label);
+        label1.showLabel(newLabel);
+        newDiv.appendChild(newLabel);
+        newDiv.appendChild(newInput);
         el.appendChild(newDiv);
     }
 }
-let firstQuestionLabel = document.getElementById('first-question-label');
-let testDiv = document.getElementById('test');
-console.log(typeof (testDiv));
-let inputTest = new InputField;
-inputTest.label = firstQuestionLabel.innerHTML;
-inputTest.render(testDiv);
+let form = document.querySelector('form');
+let inputTest = new InputField();
+inputTest.label = 'Etykieta testowa1';
+inputTest.render(form);
 //# sourceMappingURL=InputField.js.map
--- src/InputField.ts
+++ src/InputField.ts
@@ -1,4 +1,4 @@
-import { FieldLabel } from './FiledLabel'
+import { FieldLabel } from './FiledLabel';
 
 class InputField implements Field{
     name: string;
@@ -6,13 +6,25 @@ class InputField implements Field{
     fieldType: FieldTypes = FieldTypes.TextBox ;
     value: string;
     render(el: HTMLElement): void {
-        let label1 = new FieldLabel(this.label);
-        label1.showLabel(el);
+        let newDiv = document.createElement('div');
+        let newLabel = document.createElement('label');
         let newInput = document.createElement('input');
+
+        let label1 = new FieldLabel(this.label);
+        label1.showLabel(newLabel);
+        newDiv.appendChild(newLabel);
+        newDiv.appendChild(newInput);
+        el.appendChild(newDiv);
         
     }
 
 }
+
+let form = document.querySelector('form');
+
+let inputTest = new InputField();
+inputTest.label = 'Etykieta testowa1'
+inputTest.render(form);
 
 // let firstQuestionLabel = document.getElementById('first-question-label');
 // let testDiv = document.getElementById('test');
