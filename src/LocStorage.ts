179a332eff008a4942bdae9633854234e88a31ab
karolinaJot <amicus.karo@gmail.com>
2020-05-04 22:34:25 +0200

    improve use of localStorage in LocStorage class  (point 1,2 from instruction).

--- src/LocStorage.ts
+++ src/LocStorage.ts
@@ -2,11 +2,14 @@ import { MyStorage} from './MyStorage'
 
 class LocStorage implements MyStorage{
 
-    docIDList: string[] = []; 
     saveDocument(el: any): string {
         let docID = Date.now().toString(); 
         localStorage.setItem(`document${docID}`, JSON.stringify(el));
-        this.docIDList.push(`document${docID}`);
+        let docIDList = localStorage.getItem('docIDList');
+        let existingDocIDList = docIDList ? docIDList.split(',') : [];
+        existingDocIDList.push(`document${docID}`);
+        localStorage.setItem('docIDList', existingDocIDList.toString());
+
         return `document${docID}`;
     }
     loadDocument(documentID: string): any {
@@ -14,7 +17,13 @@ class LocStorage implements MyStorage{
         return JSON.parse(retrievedObject);
     }
     getDocuments(): string[] {
-        return this.docIDList;
+        let docIDList = localStorage.getItem('docIDList');
+        if(!docIDList){
+            console.log('Go home. There is no ID array.'); 
+        }else {
+            let existingDocIDList = docIDList.split(',');
+            return existingDocIDList;
+        }
     }
 
 }
