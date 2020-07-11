e144f40fa5d7092df8279ad77237063d3f049324
karolinaJot <amicus.karo@gmail.com>
2020-07-11 20:09:08 +0200

    add styles to document-list

--- src/DocumentList.ts
+++ src/DocumentList.ts
@@ -14,12 +14,21 @@ export class DocumentList {
             let list: HTMLElement = document.createElement('ul');
             for(let i = 0; i < this.docList.length; i ++){
                 let listItem: HTMLElement = document.createElement('li');
+                listItem.classList.add('list_item');
                 listItem.innerHTML = this.docList[i];
+                let spanItem:HTMLElement = document.createElement('span');
+                spanItem.classList.add('list_item__delete_doc');
+                spanItem.innerHTML = 'Usuń dokument';
+                spanItem.addEventListener('click', this.deleteDocument);
+                listItem.appendChild(spanItem);
                 list.appendChild(listItem); 
             }
             docListContainer.appendChild(list);
         }
        
+    }
+    private deleteDocument(){
+        console.log('usuń wybrany element');
     }
 
 
